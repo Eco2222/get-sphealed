81a1d10ef0799c21a042228f7bad57adb884cade
Eco2222 <163062572+Eco2222@users.noreply.github.com>
2024-05-02 20:33:42 +0000

    a

--- app/main.js
+++ app/main.js
@@ -1,6 +1,8 @@
 let fly = {};
 fly.y = 0;
 fly.x = 0;
+fly.vx = 0;
+fly.vy = 0;
 let key = new Proxy({}, {
     get(target, prop, receiver) {
         return target[prop] ?? (false);
@@ -17,10 +19,26 @@ document.onkeyup = (e) => {
 }
 function gameLoop() {
     requestAnimationFrame(gameLoop);
-    if(key.ArrowUp) {
-        
+    
+    fly.vy -= (key.ArrowUp - key.ArrowDown);
+    fly.vx += (key.ArrowRight - key.ArrowLeft);
+    
+    fly.vx *= .9;
+    fly.vy *= .9;
+    fly.y += fly.vy;
+    fly.x += fly.vx;
+    if (fly.x > $(window).width()) {
+        fly.x = -$('#plain').width();
     }
-    $('#main').css()
-
+    if (fly.x < -$('#plain').width()) {
+        fly.x = $(window).width();
+    }
+    if (fly.y > $(window).width()) {
+        fly.y = -$('#plain').width();
+    }
+    if (fly.y < -$('#plain').width()) {
+        fly.y = $(window).width();
+    }
+    $('#plain').css({'top':fly.y,'left':fly.x});
 }
 gameLoop();
